db992fc3f5cf40e7fad88381a5655a0b8dfe4c7e
Jentan Bernardus <jentan@kreaville.com>
2021-05-15 08:44:41 -0400

    improvements

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This is a [Next.js](https://nextjs.org/) and [Tailwind CSS](https://tailwindcss.com/) (v2.1) starter kit. Optimized for blazing-fast performance. 
 
----
+
 
 [![Maintainability](https://api.codeclimate.com/v1/badges/0643edcafca6909a974b/maintainability)](https://codeclimate.com/github/jentanbernardus/nextail/maintainability) [![Codeship Status for jentanbernardus/nextail](https://app.codeship.com/projects/8e864635-e0f7-4879-9ae9-f9255bf2aaf5/status?branch=main)](https://app.codeship.com/projects/444469)
 
@@ -12,12 +12,19 @@ This is a [Next.js](https://nextjs.org/) and [Tailwind CSS](https://tailwindcss.
 - [`Just-in-Time Mode`](https://tailwindcss.com/docs/just-in-time-mode) for Tailwind CSS.
 - [PostCSS](https://postcss.org/)
 - [PurgeCSS](https://purgecss.com/)
-- [cssnano](https://cssnano.co/) (coming soon)
+- [cssnano](https://cssnano.co/) (wip)
 - Deployed with [Vercel](https://vercel.com/)
 
 
 [View demo](https://nextail.vercel.app/)
 
-[![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/new/git/external?repository-url=https://github.com/jentanbernardus/nextail)
+<!-- [![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/new/git/external?repository-url=https://github.com/jentanbernardus/nextail)  -->
+
+[![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/new/git/external?repository-url=https://github.com/jentanbernardus/nextail&project-name=nextail&repo-name=nextail&demo-title=NexTail&demo-description=Next.js+%2B+Tailwind+CSS+starter+kit.+Optimized+for+blazing-fast+performance.&demo-url=https://nextail.vercel.app&demo-image=https%3A%2F%2Fbigcommerce-demo-asset-ksvtgfvnd.vercel.app%2Fbigcommerce.png&integration-ids=oac_MuWZiE4jtmQ2ejZQaQ7ncuDT) 
+
+<!-- [![Deploy to Netlify](https://www.netlify.com/img/deploy/button.svg)](https://app.netlify.com/start/deploy?repository=https://github.com/jentanbernardus/nextail) -->
 
 <!-- [Vercel Documentation](https://nextjs.org/docs/deployment) -->
+
+
+
--- pages/index.js
+++ pages/index.js
@@ -5,31 +5,28 @@ export default function Home() {
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
         <title>NextTail</title>
+        <meta name="description" content="Next.js + Tailwind CSS starter kit optimized for blazing-fast performance."/>
         {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
 
       <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
+        <h1 className="text-xl md:text-3xl lg:text-6xl font-thin">
           NexTail
         </h1>
-        <div className="p-2 my-3 bg-indigo-300 text-indigo-800 rounded-md">
+        <div className="p-3 mt-5 bg-gray-200 text-gray-800 rounded-md">
           <p className="text-sm">
-          Next.js + Tailwind CSS starter kit.Optimized for blazing-fast performance.
+            <code className="font-mono text-sm">
+          Next.js + Tailwind CSS starter kit optimized for blazing-fast performance.
+            </code>
           </p>
         </div>
-
-        <p className="mt-3 text-gray-500">
-          Get started by editing{' '}
-          <code className="px-2 font-mono text-lg bg-gray-200 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-         
+        <div className="mt-10">
+          <a href="https://vercel.com/new/git/external?repository-url=https%3A%2F%2Fgithub.com%2Fjentanbernardus%2Fnextail&project-name=nextail&repo-name=nextail&demo-title=NexTail&demo-description=A%20Next.js%20%2B%20TailwindCSS%20starter%20kit%20optimized%20for%20blazing-fast%20performance.&demo-url=https%3A%2F%2Fnextail.vercel.app"><img src="https://vercel.com/button" alt="Deploy with Vercel" /></a>
+          {/* <a href="https://app.netlify.com/start/deploy?repository=https://github.com/jentanbernardus/nextail"><img src="https://www.netlify.com/img/deploy/button.svg" alt="Deploy to Netlify" /></a> */}
         </div>
+        
       </main>
-
+      
     </div>
   )
 }
